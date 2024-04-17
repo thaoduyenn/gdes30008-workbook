b648773dc82558e8c77e4540e9f3a9fed3a37429
Jen Nguyen <thaoduyenn@student.unimelb.edu.au>
2024-04-18 01:03:38 +1000

    update

--- homepagep5.js
+++ homepagep5.js
@@ -1,17 +1,24 @@
+let laserBlast, ship;
+
+function preload() {
+	laserBlast = loadSound("assets/laser.wav");
+	ship = loadImage("assets/spaceship.png");
+}
+
 function setup() {
 	var canvas = createCanvas(windowWidth, windowHeight);
 	canvas.parent("home-canvas");
 
-	spaceship = new Sprite(
-		windowWidth - windowWidth * 0.0675,
-		windowHeight - windowHeight * 0.125
-	);
+	var shipx = windowWidth - windowWidth * 0.0675;
+	var shipy = windowHeight - windowHeight * 0.125;
 
-	// Sprite from https://millionthvector.blogspot.com/p/free-sprites.html.
+	// Spaceship sprite from https://millionthvector.blogspot.com/p/free-sprites.html.
+	spaceship = new Sprite(shipx, shipy);
 
-	spaceship.img = "assets/spaceship.png";
+	spaceship.img = ship;
 	spaceship.scale = windowHeight / 1250;
 	spaceship.collider = "none";
+	spaceship.layer = 2;
 
 	// Soundforms
 	mic = new p5.AudioIn();
@@ -26,17 +33,27 @@ function draw() {
 
 	// laser shoots from ship on press
 	if (mouse.presses()) {
-		laser = new Sprite(
-			windowWidth - windowWidth * 0.0675,
-			windowHeight - windowHeight * 0.125
-		);
+		var laserx = windowWidth - windowWidth * 0.0675;
+		var lasery = windowHeight - windowHeight * 0.125;
+		laser = new Sprite(laserx, lasery);
 		laser.w = 7;
 		laser.h = 50;
 		laser.color = "#2dff19";
 		laser.strokeWeight = 0;
 		laser.collider = "none";
+		laser.layer = 1;
+
+		// Laser life proportional to distance from mouse.
+
+		var d = dist(laserx, lasery, mouse.x, mouse.y);
+		var l = map(d, 0, dist(laserx, lasery, 0, 0), 0, 120);
+		laser.life = l;
+
 		laser.rotateMinTo(mouse, 20, 90);
 		laser.moveTo(mouse.x, mouse.y, 20);
+
+		// Play laser blast
+		laserBlast.play();
 	}
 
 	let spectrum = fft.analyze();
